8008f1aeb51defefc13c2abaf005332cd55b6d01
Hector Blondel <blondel.hector@gmail.com>
2025-02-01 20:42:28 +0100

    fixed " in strings

--- test_execute_code.py
+++ test_execute_code.py
@@ -6,9 +6,10 @@ from query_openai import query_llm
 with open("moving_api.py", "r") as file:
     python_input = file.read() + "\n" + "\n" + query_llm("The robot moves forwards 10 times and backwards 5 times")
 
-command = "python3 -c {}".format(python_input)
-print("command is : ", command)
+command = "python3 -c \"{}\"".format(python_input)
+print("COMMAND : ", command)
 result = subprocess.run(command, shell=True, capture_output=True, text=True).stdout
+print("RESULT : ")
 print(result)
 
 
