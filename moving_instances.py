0541367f48dc4e292e22f4952100dfc91d3851f9
Hector Blondel <blondel.hector@gmail.com>
2025-02-01 19:53:30 +0100

    added moving functions

---
+++ moving_instances.py
@@ -0,0 +1,15 @@
+def forwards():
+    """The robot moves forward"""
+    print("FORMWARD")
+
+def backwards():
+    """The robot moves backward"""
+    print("BACKWARDS")
+
+def left():
+    """The robot moves left"""
+    print("LEFT")
+
+def right():
+    """The robot moves right"""
+    print("RIGHT")
--- query_openai.py
+++ query_openai.py
@@ -1,35 +1,35 @@
 import openai
-
 import os
-
 from dotenv import load_dotenv
 
 
-load_dotenv()
+def query_llm(prompt):
+    """
+    Queries openai API and returns a ChatGPT python generated file for getting 
+    the machine instructions (FORWARDS, BACKWARDS, LEFT, RIGHT)
+    """
+    
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    client = openai.OpenAI(api_key=openai_api_key)
+    prompt = "While there is no boundary, go forward"
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
+    with open("moving_instances.py", "r") as file:
+        moving_definition = file.read()
+    
+    messages=[
+    {"role": "system", "content": "You are a coding assistant that only outputs function code in python.\
+      You do not explain the code, only give the function, and do not include comments."},
+    {"role": "user", "content": f"Write a python code in one script for the instruction \"{prompt}\"\
+     Only the following functions are allowed : \npython{moving_definition}\
+     Only the following keywords are allowed : if, else, for"}
+    ]
+
+    response = client.chat.completions.create(model="gpt-4o",messages=messages)
+    print(messages)
+
+    print(response.choices[0].message.content)
 
 
-client = openai.OpenAI(api_key=openai_api_key)
-
-
-prompt = "While there is no boundary, go forward"
-
-
-
-response = client.chat.completions.create(
-
-model="gpt-4o",
-
-messages=[
-
-{"role": "system", "content": "You are a coding assistant that only outputs function code in python. You do not explain the code, only give the function, and do not include comments."},
-
-{"role": "user", "content": f"Write the python code for {prompt}"}
-
-]
-
-)
-
-
-print(response.choices[0].message.content)
+if __name__ == "__main__":
+    query_llm("The robot moves forwards 10 times and backwards 5 times")
