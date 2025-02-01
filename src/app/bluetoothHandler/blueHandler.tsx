8367be548c2e0b064b7b9069658388f205bf1234
aniket1101 <1.aniketgupta@gmail.com>
2025-02-01 22:17:08 +0000

    feat: Added bluetooth handler and label for connection status

---
+++ src/app/bluetoothHandler/blueHandler.tsx
@@ -0,0 +1,38 @@
+/// <reference types="web-bluetooth" />
+
+let device: BluetoothDevice;
+
+async function bluetoothInit() {
+    if (!navigator.bluetooth) console.log("Error in connecting");
+    if (!device) await requestDevice();
+  
+    const connectButton = document.querySelector("ConnectButton");
+    if (connectButton) {
+      connectButton.textContent = "connecting...";
+    }
+
+    await connectDevice();
+  }
+
+  async function requestDevice() {
+    const options = {
+      /* UUID stuff goes here... */
+      acceptAllDevices: true,
+    };
+    device = await navigator.bluetooth.requestDevice(options);
+    device.addEventListener("gattserverdisconnected", connectDevice);
+  }
+  
+  async function connectDevice() {  
+    if (!device || !device.gatt) {
+      throw new Error('Device or GATT server is not available');
+    }
+
+    const server = await device.gatt.connect();
+  
+    console.log("connected");
+  }
+
+  async function getDevice(): Promise<BluetoothDevice> {
+    return device;
+  }
--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -2,55 +2,46 @@
 
 import { FormEventHandler } from 'react';
 import './connect.css'
-/// <reference types="web-bluetooth" />
+import './bluetoothHandler/blueHandle.tsx'
+import { useState } from 'react';
 
-let device: BluetoothDevice;
+var device: BluetoothDevice;
 
 export default function Connect() {
-
-  async function init() {
-    if (!navigator.bluetooth) console.log("Error in connecting");
-    if (!device) await requestDevice();
   
-    const connectButton = document.querySelector("ConnectButton");
-    if (connectButton) {
-      connectButton.textContent = "connecting...";
-    }
-
-    await connectDevice();
-  }
-
-  async function requestDevice() {
-    const options = {
-      acceptAllDevices: true,
-    };
-    device = await navigator.bluetooth.requestDevice(options);
-    device.addEventListener("gattserverdisconnected", connectDevice);
-  }
-  
-  async function connectDevice() {  
-    if (!device || !device.gatt) {
-      throw new Error('Device or GATT server is not available');
-    }
-
-    const server = await device.gatt.connect();
-  
-    console.log("connected");
-  }
-
-  const connectToBluetooth = () => {
-    init().catch
-  }
-  
-
   return (
     <div>
       <h1 className="ConnectPageTitle"> Connect Page </h1>
       <input name="PromptInput" id=""></input>
       <textarea name="ScriptOututTextArea" id=""></textarea>
+
+    </div>
+  );
+}
+
+function ConnectPanel() {
+
+  const [connectionStatus, setConnectionStatus] = useState("Not connected");
+
+  const connectToBluetooth = () => {
+    bluetoothInit().then
+    getDevice().then((dev) => {
+      device = dev;
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  var deviceIsConnected: boolean = !!device.id
+
+  return (
+    <div className="ConnectPanel">
       <button className="ConnectButton" id='ConnectButton' onClick={connectToBluetooth}>
         Connect
       </button>
+      <label className="ConnectionStatus">
+        {device.id}
+      </label>
     </div>
-  );
+  )
 }
