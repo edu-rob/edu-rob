03022d64de0c7811a3de29759687c142718a1874
Hector Blondel <blondel.hector@gmail.com>
2025-02-02 13:32:03 +0100

    added logo in front page

--- frontend/src/app/components/navBar/navBar.tsx
+++ frontend/src/app/components/navBar/navBar.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import "./navBar.css";  
+import Image from "next/image";
 
 export default function NavBar() {
-
     return (
-    <div className="NavBarWrapper">
-        <div className="NavBar">
-                <h1 className='NavBarTitleText'>
-                    EduRob
-                </h1>
-        </div>    
-    </div>
-    )
+        <div className="NavBarWrapper">
+            <div className="NavBar">
+                <h1 className="NavBarTitleText">EduRob</h1>
+                <img src="./logo.png" alt="" className="NavBarLogo" />
+            </div>    
+        </div>
+    );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,3 +28,4 @@ export default function RootLayout({ children }: Readonly<{children: React.React
     </html>
   );
 }
+  
